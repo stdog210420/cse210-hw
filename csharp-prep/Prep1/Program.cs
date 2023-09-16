42ff0136b51aae5753fa921bd0ded987162e5470
stdog210420 <lin23013@byui.edu>
2023-09-16 22:23:07 +0800

    added to my homework

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        String name1 = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        String name2 = Console.ReadLine();
+        Console.WriteLine($"Your name is {name1}, {name1} {name2}.");
+
+
     }
 }
