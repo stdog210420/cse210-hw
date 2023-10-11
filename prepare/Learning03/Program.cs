52c7f214f0d6e506d653d451ce035e53e8fc83c2
stdog210420 <lin23013@byui.edu>
2023-10-12 00:53:44 +0800

    added to my homework

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,50 +1,71 @@
 using System;
+using System.Diagnostics;
 using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-                Fraction f1 = new Fraction();
-        Fraction f2 = new Fraction(6);
-        Fraction f3 = new Fraction(6,7);
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction(3,4);
+        Fraction f4 = new Fraction(3,4);
+        f4.SetTop(1);
+        f4.Setbottom(3);
+        Console.WriteLine($"{f1.GetFractionString()}");
+        Console.WriteLine($"{f1.GetDecimalValue()}");
+        Console.WriteLine($"{f2.GetFractionString()}");
+        Console.WriteLine($"{f2.GetDecimalValue()}");
+        Console.WriteLine($"{f3.GetFractionString()}");
+        Console.WriteLine($"{f3.GetDecimalValue()}");
+        Console.WriteLine($"{f4.GetFractionString()}");
+        Console.WriteLine($"{f4.GetDecimalValue()}");
     }
 }
 public class Fraction
 {
     private int _top;
     private int _bottom;
-
-    private float _fraction;
-    public Fraction(){
-        _fraction = 1/1;
+    public Fraction()
+    {    
+        _top = 1;
+        _bottom = 1;
     }
     public Fraction(int wholeNumber)
     {
-        _bottom = 1;
-        _top = wholeNumber;
-        _fraction = wholeNumber / _bottom;
+        _top = wholeNumber ;
+        _bottom = 1 ;
     }
     public Fraction(int top, int bottom)
     {
-        _fraction = _top / _bottom;
+        _top = top ;
+        _bottom = bottom ;
     }
-    public void GetTop(){
-
+    public int GetTop(){
+        return _top;
+    }
+    public int GetBottom(){
+        return _bottom;
     }
     public void SetTop(int top){
-        
+        _top = top;        
     }
-    public void bottom(){
+    public void Setbottom(int bottom){
+        if (bottom != 0)
+        {
+            _bottom = bottom;
+        }
+        else
+        {
+            Console.WriteLine($"Cannot divide by zero. Please enter a number except zero.");
+        }
+    }
+    public string GetFractionString(){
+            string _fraction = $"{_top} / {_bottom}";
+            return _fraction;
+    }
+    public double GetDecimalValue(){
+            return (double) _top / (double)_bottom;
 
     }
-    public void Setottom(int bottom){
-        
-    }
-    // public GetFractionString(){
-
-    // }
-    // public GetDecimalValue(){
-
-    // }
 }       
