6a67d84b35d29d6f907f22003b60db85071bb43e
stdog210420 <lin23013@byui.edu>
2023-10-11 08:23:49 +0800

    added to my homepage

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+                Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6,7);
     }
 }
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    private float _fraction;
+    public Fraction(){
+        _fraction = 1/1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _bottom = 1;
+        _top = wholeNumber;
+        _fraction = wholeNumber / _bottom;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _fraction = _top / _bottom;
+    }
+    public void GetTop(){
+
+    }
+    public void SetTop(int top){
+        
+    }
+    public void bottom(){
+
+    }
+    public void Setottom(int bottom){
+        
+    }
+    // public GetFractionString(){
+
+    // }
+    // public GetDecimalValue(){
+
+    // }
+}       
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,5 +1,6 @@
 using System;
 
+using System;
 class Program
 {
     static void Main(string[] args)
