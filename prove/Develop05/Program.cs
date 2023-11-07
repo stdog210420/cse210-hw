ba9fd9e78184ccdc9791823c9fe7e85b99721d88
stdog210420 <lin23013@byui.edu>
2023-11-08 01:18:24 +0800

    added to my homework

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        // Design Requirements
+        Simple mySimple = new Simple();
+        Eternal myEternal = new Eternal();
+        Checklist myChecklist = new Checklist();
+        Tracker myTracker = new Tracker();
+        Goal myGoal = new Goal();
+        string  _choice = "0";
+        int i = 0; int j = 0; int k =0;
+
+        while (_choice != "6")
+        {
+
+            Console.ForegroundColor = ConsoleColor.Blue;            
+            myTracker.TrackerOptions();
+            _choice = Console.ReadLine();
+            switch(_choice)
+            {
+                // choice 1. Breathing Activity
+                case "1":
+                {
+                    myBreathing.BreathingActivity();
+                    i ++;
+                    
+                    break;
+                }
+                // choice 2. Reflection Activity
+                case "2":
+                {                
+                    myReflection.ReflectionActivity();
+                    j ++;
+
+                    break;
+                }
+                // choice 3. Listing Activity  
+                case "3":
+                {
+                    myListing.ListingActivity();
+                    k ++;  
+                    break;
+                }
+                // choice 4. Quit  
+                case "4":
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                    Console.WriteLine("Already Quit!");
+                    break;
+                }
+            }
+
+        }
     }
 }
---
+++ prove/Develop05/Tracker.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Tracker
+{
+    public void TrackerOptions()
+    {   
+        Console.ForegroundColor = ConsoleColor.Blue;    
+        Console.WriteLine("Menu Options:");              
+        Console.WriteLine("  1. Create New Goal");
+        Console.WriteLine("  2. List Goals");
+        Console.WriteLine("  3. Save Goals");
+        Console.WriteLine("  4. Load Goals");
+        Console.WriteLine("  5. Load Goals"); 
+        Console.WriteLine("  6. Load Goals"); 
+        Console.Write("Select a choice from the menu: "); 
+    }    
+}
