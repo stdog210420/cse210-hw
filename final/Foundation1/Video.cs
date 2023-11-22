a7b06c4b0424ea342edf795f65922120974bf070
stdog210420 <lin23013@byui.edu>
2023-11-23 01:03:28 +0800

    added to my homework

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -5,13 +5,37 @@ class Video
     private string _title;
     private string _author;
     private float _length;
-    private string _comment;
-    private List<Comment> _commentList = new List<Comment>();
-    public void ReturnComment()
-    {
-
+    private List<Comment> _commentList;
+    public string GetTitle()
+    {   
+        return _title;
+    }
+    public string GetAuthor()
+    {   
+        return _author;
+    }
+    public float GetLength()
+    {   
+        return _length;
     }
 
+    public Video(string title, string author, float length)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+        _commentList = new List<Comment>();
+    }
+    public void AddComment(string author, string text)
+    {
+        Comment comment = new Comment(author, text);
+        _commentList.Add(comment);
+
+    }
+    public int NumberComment()
+    {
+        return _commentList.Count;
+    }
 
 
 }
